0ab7105764387ebf6cc98472cac1d18c1bf913b5
Eli Broadfoot <elibroadfoot@gmail.com>
2016-10-11 20:44:44 -0700

    Styled clock

--- library.js
+++ library.js
@@ -1,13 +1,18 @@
 function startTime() {
 	var today = new Date();
 	var h = today.getHours();
-	var m = today.getMinutes();
-	var s = today.getSeconds();
-	var ms = today.getMilliseconds();
+	var m = today.getMinutes().toString();
+	var s = today.getSeconds().toString();
+	var ms = today.getMilliseconds().toString().slice(0, 2);
+	var ampm = (' PM ');
+	if (h < 12) {ampm = (' AM ')};
+    if (h > 12) {h -= 12};
+	h = checkTime (h);
 	m = checkTime (m);
 	s = checkTime (s);
 	ms = checkTime (ms);
-	document.getElementById('clock').innerHTML = h + ":" + m + ":" + s + ":" + ms;
+	ampm = checkTime (ampm);
+	document.getElementById('clock').innerHTML = h + ":" + m + ":" + s + ":" + ms + ampm;
 	var t = setTimeout(startTime, 1);
 }
 function checkTime(i) {
